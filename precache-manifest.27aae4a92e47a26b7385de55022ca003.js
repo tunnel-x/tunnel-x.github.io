f18a5dbdf6b1830ab68d4bfbdb4811f595688266
Berkay Barlas <fpberkay@gmail.com>
2019-04-14 15:08:15 +0300

    Updates

---
+++ precache-manifest.27aae4a92e47a26b7385de55022ca003.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ae3e2a8c7caefcb7ff7",
+    "url": "/website/static/css/main.cc1c68fe.chunk.css"
+  },
+  {
+    "revision": "5ae3e2a8c7caefcb7ff7",
+    "url": "/website/static/js/main.78984076.chunk.js"
+  },
+  {
+    "revision": "738c5e760cdb5d1c1801",
+    "url": "/website/static/js/runtime~main.0439b053.js"
+  },
+  {
+    "revision": "94977998a524b09e81df",
+    "url": "/website/static/js/2.8a87d74f.chunk.js"
+  },
+  {
+    "revision": "48d0c0f8660f09556885770dde7bb6d5",
+    "url": "/website/static/media/logo.48d0c0f8.png"
+  },
+  {
+    "revision": "9760b1f3fde262f98eb071dd2800dc50",
+    "url": "/website/static/media/tunnelx.9760b1f3.png"
+  },
+  {
+    "revision": "8dcbcf234564b2b8810a25de8250c67d",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2293c448e000ad04ef7762e7f45a8b89.js"
+  "/website/precache-manifest.27aae4a92e47a26b7385de55022ca003.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
