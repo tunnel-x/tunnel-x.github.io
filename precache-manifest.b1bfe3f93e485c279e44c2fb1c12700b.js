34c5eb1e41814c9f9c4b275cb29e74f3a0e2bb95
Berkay Barlas <fpberkay@gmail.com>
2019-05-08 17:19:25 +0300

    Updates

---
+++ precache-manifest.b1bfe3f93e485c279e44c2fb1c12700b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4f8438f288ef98637ea",
+    "url": "/static/css/main.cb16c3b9.chunk.css"
+  },
+  {
+    "revision": "a4f8438f288ef98637ea",
+    "url": "/static/js/main.360a48f2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "65aa734308ef5504e598",
+    "url": "/static/js/2.2afd4d5f.chunk.js"
+  },
+  {
+    "revision": "48d0c0f8660f09556885770dde7bb6d5",
+    "url": "/static/media/logo.48d0c0f8.png"
+  },
+  {
+    "revision": "9760b1f3fde262f98eb071dd2800dc50",
+    "url": "/static/media/tunnelx.9760b1f3.png"
+  },
+  {
+    "revision": "a1e3ab0280395934d85ab899c103ec15",
+    "url": "/index.html"
+  }
+];
